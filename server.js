63d58162300bc920ef951e6e1a7f87f1d0db814e
evermuniz <evermuniz@outlook.com>
2023-05-10 21:59:54 -0600

    added inquirer prompt test

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const mysql = require("mysql2");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-
+const CLI = require('./lib/cli');
+const cli = new CLI();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -28,8 +29,20 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
+db.query(
+    `SOURCE schema.sql;`,
+    function (err, results) {
+  console.log('Database and tables loaded');
+});
 
-db.query(   `SELECT employee.id, 
+db.query(`SOURCE seeds.sql;`, function (err, results) {
+  console.log('Data loaded');
+});
+
+
+
+db.query(   `SELECT 
+                employee.id, 
                 employee.first_name, 
                 employee.last_name, 
                 role.title, 
@@ -44,4 +57,7 @@ db.query(   `SELECT employee.id,
             LEFT JOIN employee as manager on manager.manager_id = employee.id;`,
             function (err, results) {
                 console.table(results);
+                cli.run();
             });
+
+
